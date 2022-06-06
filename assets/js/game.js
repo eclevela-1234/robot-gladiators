ac028ec1f7727a27d5e358cf0e9559bd1642bc13
Eliot <eliothead@live.com>
2022-06-06 13:11:20 -0400

    adding messaround code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?")
+
+console.log(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+console.log("this logs a string");
+
+console.log(10+10);
+
+console.log("our robot's name is " + playerName);
